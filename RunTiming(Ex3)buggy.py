5cb9a48d63298c9ef664a288f319fb2e41a2f071
Jeffrey Scruggs <jeff.scruggs23@gmail.com>
2021-07-09 06:55:13 -0400

    added RunTiming(Ex3)buggy.py with a bug.

---
+++ RunTiming(Ex3)buggy.py
@@ -0,0 +1,20 @@
+def run_timing():
+    runs = 0
+    total_time = 0
+
+    while True:
+        runtimes = float(input("Enter 10 km run time: "))
+
+        if not runtimes:
+            break
+
+        runs += 1
+        total_time += float(runtimes)
+
+    average_time = total_time / runs
+    print(average_time)
+
+    print(f"Average of {average_time}, over {runs} runs.")
+
+run_timing()
+print("Terminating Printing...")
